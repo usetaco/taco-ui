8aec4171d0d7f5911709cdde71fae8743ca240ba
Eric Gift <eric.j.gift@gmail.com>
2023-07-15 14:47:32 -0400

    docs on main

---
+++ docs/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Flex, Grid, GridItem, Image, Text } from '@chakra-ui/react'
+import { PageHeader } from 'taco-labs-ui'
+
+const HomePage = () => {
+  return (
+    <Container
+      mt="200px"
+      py={5}
+      maxW="container.xl"
+    >
+      <PageHeader
+        title="Taco Labs UI Docs"
+        subtitle="Documentation"
+      />
+    </Container>
+  )
+}
+
+export default HomePage
